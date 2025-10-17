44de95d9f8278be564b91dbbe69de6c96e9fc4bd
Mihaly24 <fairuzk20@gmail.com>
2025-10-17 16:04:25 +0700

    Melanjutkan isi mod Image pada Box di spacer

--- app/src/main/java/com/example/myandro/tataletak.kt
+++ app/src/main/java/com/example/myandro/tataletak.kt
@@ -1,5 +1,6 @@
 package com.example.myandro
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -129,6 +131,11 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 .height(height = 300.dp)
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
-        )
+        ){
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+
+        }
     }
 }
